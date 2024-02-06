a27d62520d33d75a50a2c9f8b4ec3ea0eca79f1f
andreleao1 <andre.gustavo.ls@hotmail.com>
2024-02-05 21:49:45 -0300

    first commit

---
+++ src/main/java/br/com/agls/expensesusersapi/ExpensesUsersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agls.expensesusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesUsersApiApplication.class, args);
+	}
+
+}
